480c70df21a44f39bcd0a0db20ec42d8ed057033
Dekiru <dwyl.agency1@gmail.com>
2026-08-25 02:02:19 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')).render(
 
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+
+    <Analytics />
+  </StrictMode>
+
 )
