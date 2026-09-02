275542d25064c53746da6045b9446d9661574063
Dekiru <dwyl.agency1@gmail.com>
2026-09-02 20:39:57 +0530

    data_structure.jsx

---
+++ src/componets/data_structure.jsx
@@ -0,0 +1,67 @@
+import megumi from './img/megumi.png'
+import dead_gojo from './img/deadgojo.png'
+import yuta from './img/yuta.png'
+import chainsaw_man from './img/chainsawman.png'
+import mahoraga from './img/mahoraga.png'
+import gun_devil from './img/gundevil.png'
+import gojoeyes from './img/gojoeyes.png'
+const wallpapers = [
+    {
+        id: 1,
+        title: "Megumi Fushiguro",
+        anime: "Jujutsu Kaisen",
+        category: "Character",
+        image: megumi,
+        resolution: "1920x1080"
+    },
+    {
+        id: 2,
+        title: "Gojo Satoru",
+        anime: "Jujutsu Kaisen",
+        category: "Character",
+        image: dead_gojo,
+        resolution: "1920x1080"
+    },
+    {
+      id: 3,
+      title: "yuta",
+      anime: "Jujutsu Kaisen",
+      category: "Character",
+      image: yuta,
+      resolution: "1920x1080"
+    },
+    {
+      id: 4,
+      title: "Dengi",
+      anime: "Chainsaw man",
+      category: "Character",
+      image: chainsaw_man,
+      resolution: "1920x1080"
+    },
+    {
+    id: 5,
+    title: "Mahoraga",
+    anime: "Jujutsu Kaisen",
+    category: "Character",
+    image: mahoraga,
+    resolution: "1920x1080"
+    },
+    {
+    id: 6,
+    title: "Gun Devil",
+    anime: "Chainsaw man",
+    category: "Character",
+    image: gun_devil,
+    resolution: "1920x1080"
+    },
+    {
+    id: 7,
+    title: "Gojo",
+    anime: "Jujutsu Kaisen",
+    category: "Character",
+    image: gojoeyes,
+    resolution: "1920x1080"
+    },
+    
+];
+export default wallpapers
